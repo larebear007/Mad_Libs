98494157eb50b9f1ab268608f8ea7014df083d6c
larebear007 <larencozart@gmail.com>
2022-03-21 19:32:11 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# MAD LIBS
+
+# SPECIFICATIONS: Create a program that contains a script with some lines left blank for user input.
+# Prompt the user for input to fill in the blanks, asking for appropriate parts of speech.
+# After the user has entered in their contents, print out the story script with their entries to reveal
+# their unique (and probably hilarious) story. Advanced Features: 1. A loop that allows them to play the
+# game continuously, 2. A menu bar with 2-3 story templates to choose from, 3. color-coded words to
+# differentiate the user-input words from the story script.
+
